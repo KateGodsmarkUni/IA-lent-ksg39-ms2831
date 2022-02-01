a43c562584efce29e435baecb6bc78a69a7504ca
Mridhu <msrideviprabhakar1005@gmail.com>
2022-02-01 17:18:38 +0000

    Completed test for Task 1F - added assert to testing monitoring station, added test for inconsistent list function

--- test_station.py
+++ test_station.py
@@ -3,8 +3,8 @@
 # SPDX-License-Identifier: MIT
 """Unit test for the station module"""
 
-from floodsystem.station import MonitoringStation
-
+from floodsystem.station import MonitoringStation, inconsistent_typical_range_stations
+from floodsystem.stationdata import build_station_list
 
 def test_create_monitoring_station():
 
@@ -25,3 +25,17 @@ def test_create_monitoring_station():
     assert s.typical_range == trange
     assert s.river == river
     assert s.town == town
+    assert s.typical_range_consistent() == True
+
+def test_inconsistent_typical_range_stations():
+    
+    #Get stations list and find number of inconsistent stations
+    stations = build_station_list()
+    num_inconsistent_stations = 0
+    for station in stations:
+        if station.typical_range_consistent() == False:
+            num_inconsistent_stations += 1
+    
+    #Check result of inconsistent stations function
+    inconsistent_stations = inconsistent_typical_range_stations(stations)
+    assert len(inconsistent_stations) == num_inconsistent_stations
