fd5c6d073adff7be150c7eb4cd882945d6d9c91d
Kate Godsmark <ksg39@cam.ac.uk>
2022-02-10 11:02:15 +0000

    added tests for flood.py

--- floodsystem/flood.py
+++ floodsystem/flood.py
@@ -13,11 +13,11 @@ def stations_level_over_threshold(stations, tol):
     return sorted_by_key(high_stations, 1, reverse=True)
 
 
-def stations_highest_rel_level(stations, N):
+def stations_highest_rel_level(stations, n):
     ''' Function that returns a list of the N stations (objects) with the highest relative water level.'''
     all_stations = stations_level_over_threshold(stations, 0)
     highest_n = []
-    for i in range(N):
+    for i in range(n):
         highest_n.append(all_stations[i][0])
     return highest_n
 
---
+++ test_flood.py
@@ -0,0 +1,32 @@
+"""Unit test for the flood module"""
+from floodsystem.flood import *
+from floodsystem.stationdata import build_station_list, update_water_levels
+
+
+def test_stations_level_over_threshold():
+    
+    # Build list of stations
+    stations = build_station_list()
+
+    # Update latest level data for all stations
+    update_water_levels(stations)
+
+    stations_above = stations_level_over_threshold(stations, 0.9)
+    for i in range(len(stations_above) - 1):
+        assert stations_above[i][1] > stations_above[i + 1][1]
+        assert stations_above[i][1] >= 0.9
+
+
+def test_stations_highest_rel_level():
+
+     # Build list of stations
+    stations = build_station_list()
+
+    # Update latest level data for all stations
+    update_water_levels(stations)
+
+    highest_n = stations_highest_rel_level(stations, 10)
+    assert len(highest_n) == 10
+
+    for i in range(len(highest_n) - 1):
+        assert highest_n[i].relative_water_level() > highest_n[i + 1].relative_water_level()
